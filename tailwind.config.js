61c8f443cc580500cb82c9cce612a05f91353f75
Eduardo Ezequiel <153006901+edurepubli@users.noreply.github.com>
2025-10-16 17:48:43 -0600

    feat: customize styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#7A7AF9",
+                secondary: "#1D1F49",
+                background: "#0E1024",
+                textMuted: "#B7B9CA",
+                border: "#38394D",
+                inputBackground: "#141633",
             },
         },
     },
